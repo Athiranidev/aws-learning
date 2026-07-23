3306c41619f11f7213605f3f7a628e7f5339c6d3
Athiranidev <athirani.official@gmail.com>
2026-07-23 12:30:28 +0530

    Add VPC networking with Internet Gateway and Route Table

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -25,6 +25,7 @@ resource "aws_subnet" "public_subnet" {
     Name = "public-subnet"
   }
 }
+
 resource "aws_subnet" "private_subnet" {
   cidr_block        = "10.0.2.0/24"
   vpc_id            = aws_vpc.main.id
@@ -34,4 +35,27 @@ resource "aws_subnet" "private_subnet" {
   }
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
 
+  tags = {
+    Name = "Internet-gateway"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+    vpc_id = aws_vpc.main.id
+ route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+    route_table_id  = aws_route_table.public_rt.id
+    subnet_id      = aws_subnet.public_subnet.id
+}
